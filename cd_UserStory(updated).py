c892925e0d5e793b082fc18b9e09ceed724aafdd
Artur Sargsyan <48749389+ArturSargsyans@users.noreply.github.com>
2019-05-17 10:11:36 +0400

    Update cd_UserStory(updated).py

--- cd_UserStory(updated).py
+++ cd_UserStory(updated).py
@@ -8,25 +8,24 @@ def loadCalculusDictionary():
 
 def introduction(chapters):
     while True:
-        startorclose = input("\nInsert 'start' to open the chapters")
-        if startorclose == 'start':
+        startorclose = input("\nInsert 'start' to open the chapters, insert 'close' to exit")
+        if startorclose == 'start' or startorclose == 'close':
             decisionandchapters = (startorclose, chapters)
             break
         else:
             print("PLEASE FOLLOW THE INSTRUCTIONS")
     return decisionandchapters
 
-
 def chooseTheChapter(chapters):
     print("Here are the chapters available")
     for key in chapters[1]:
         print("\n", key, "\n")
     while True:
-        chaptersname = input("\nPlease enter the name of the chapter that you want to examine. To exit type 'close'")
+        chaptersname = input("\nPlease enter the name of the chapter that you want to examine. To go back type 'back'")
         if chaptersname in chapters[1]:
             chapters = chapters[1][chaptersname]
             break
-        elif chaptersname == 'close':
+        elif chaptersname == 'back':
             break
         else:
             print("\nTHAT CHAPTER DOES NOT EXIST!, please try again")
@@ -38,11 +37,11 @@ def chooseTheCategory(chapter):
         print("Here are the categories")
         for categories in chapter[1]:
             print("\n", categories, "\n")
-        categoryname = input("\nplease input whatever you want to access. if you want to exit type 'close'")
+        categoryname = input("\nplease input whatever you want to access. If you want to go back to chapters type 'back'")
         if categoryname in chapter[1]:
             chapter = chapter[1][categoryname]
             break
-        elif categoryname == 'close':
+        elif categoryname == 'back':
             break
         else:
             print("\nThere is no such category. Please try again")
@@ -54,8 +53,8 @@ def openTheDescription(category):
     while True:
         for description in category[1]:
             print("\n", description, "\n")
-        descriptionname = input("\nType the name of the concept that you want to learn. If you want to exit type 'close'")
-        if descriptionname == 'close':
+        descriptionname = input("\nType the name of the concept that you want to learn. If you want to go back to categories type 'back'")
+        if descriptionname == 'back':
             break
         elif descriptionname in category[1]:
             category = category[1][descriptionname]
@@ -65,28 +64,33 @@ def openTheDescription(category):
     decisionanddescription = (descriptionname, category)
     return decisionanddescription
 
-
-
-
-
-
 def main():
     initialcalculusDictionary = loadCalculusDictionary()
     while True:
-        calculusDictionary = introduction(initialcalculusDictionary)
-        calculusDictionary = chooseTheChapter(calculusDictionary)
-        if calculusDictionary[0] == 'close':
+        calculusDictionary1 = introduction(initialcalculusDictionary)
+        if calculusDictionary1[0] == 'close':
             break
         else:
-            calculusDictionary = chooseTheCategory(calculusDictionary)
-            if calculusDictionary[0] == 'close':
-                break
-            else:
-                calculusDictionary = openTheDescription(calculusDictionary)
-                if calculusDictionary[0] == 'close':
+            while True:
+                calculusDictionary2 = chooseTheChapter(calculusDictionary1)
+                if calculusDictionary2[0] == 'back':
                     break
                 else:
-                    print(calculusDictionary[1])
-
+                    while True:
+                        calculusDictionary3 = chooseTheCategory(calculusDictionary2)
+                        if calculusDictionary3[0] == 'back':
+                            break
+                        else:
+                            while True:
+                                calculusDictionary4 = openTheDescription(calculusDictionary3)
+                                if calculusDictionary4[0] == 'back':
+                                    break
+                                else:
+                                    print(calculusDictionary4[1])
+                                    while True:
+                                        if input("\ntype 'back' to go back") == 'back':
+                                            break
+                                        else:
+                                            print("\nPLEASE FOLLOW THE INSTRUCTIONS")
 
 main()
